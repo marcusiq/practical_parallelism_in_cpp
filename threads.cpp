b2da2bd573ee2e22a5a4745253dd9bfd06a0ad36
MarcusIQ <marcus.iqbal@gmail.com>
2021-02-27 21:13:19 -0500

    This is the first threads.cpp

---
+++ threads.cpp
@@ -0,0 +1,23 @@
+// This program prints from multiple threads using c++11
+// by marcus with help by coffeebeforearch
+
+#include <iostream>
+#include <thread>
+
+void print_func(int id) {
+	std::cout << "Printing from threads" << id << '\n';
+}
+
+int main() {
+	// Create 4 threads that all call print function
+	std::thread t0(print_func, 0);
+	std::thread t1(print_func, 1);
+	std::thread t2(print_func, 2);
+	std::thread t3(print_func, 3);
+
+	// wait for the threads to finish
+	t0.join();
+	t1.join();
+	t2.join();
+	t3.join();
+}
